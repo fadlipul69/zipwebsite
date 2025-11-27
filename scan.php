63c6fbc1ceb8db568523a47a3f617d66eeace7f6
fadlipul69 <fadlipul69@gmail.com>
2025-11-27 19:14:09 +0700

    Update scan.php

--- scan.php
+++ scan.php
@@ -1,109 +1,482 @@
+<?php
+/**
+Written by Cvar1984 <Cvar1984@pm.me>, November 2022
+Copyright (C) 2022  Cvar1984
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+$minute = 15;
+$limit = (60 * $minute); // 60 (seconds) = 1 Minutes
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', $limit);
+set_time_limit($limit);
+/**
+ * Recursive listing files
+ *
+ * @param string $directory
+ * @param array $entries_array optional
+ * @return array of files
+ */
+function recursiveScan($directory, &$entries_array = array())
+{
+    // link can cause endless loop
+    $handle = @opendir($directory);
+    if ($handle) {
+        while (($entry = readdir($handle)) !== false) {
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+            $entry = $directory . DIRECTORY_SEPARATOR . $entry;
+            if (is_dir($entry) && is_readable($directory) && !is_link($directory)) {
+                $entries_array = recursiveScan($entry, $entries_array);
+            } elseif (is_file($entry) && is_readable($entry)) {
+                $entries_array['file_writable'][] = $entry;
+            } else {
+                $entries_array['file_not_writable'][] = $entry;
+            }
+        }
+        closedir($handle);
+    }
+    return $entries_array;
+}
+/**
+ *
+ * Sort array of list file by lastest modified time
+ *
+ * @param array  $files Array of files
+ *
+ * @return array
+ *
+ */
+function sortByLastModified($files)
+{
+    @array_multisort(array_map('filemtime', $files), SORT_DESC, $files);
+    return $files;
+}
+/**
+ *
+ * Recurisively list a file by descending modified time
+ *
+ * @param string $path
+ *
+ * @return array
+ *
+ */
+function getSortedByTime($path)
+{
+    $result = recursiveScan($path);
+    $fileWritable = $result['file_writable'];
+    $fileNotWritable = isset($result['file_not_writable']) ? !$result['file_not_writable'] : false;
+    $fileWritable = sortByLastModified($fileWritable);
+
+    return array(
+        'file_writable' => $fileWritable,
+        'file_not_writable' => $fileNotWritable
+    );
+}
+/**
+ * Recurisively list a file by array of extension
+ *
+ * @param string $path
+ * @param array $ext
+ * @return array of files
+ */
+function getSortedByExtension($path, $ext)
+{
+    $result = getSortedByTime($path);
+    $fileWritable = $result['file_writable'];
+    isset($result['file_not_writable']) ? $result['file_not_writable'] : false;
+
+    foreach ($fileWritable as $entry) {
+        $pathinfo = pathinfo($entry, PATHINFO_EXTENSION);
+        $pathinfo = strtolower($pathinfo);
+
+        if (in_array($pathinfo, $ext)) {
+            $sortedWritableFile[] = $entry;
+        }
+    }
+    if (isset($fileNotWritable)) {
+        foreach ($fileNotWritable as $entry) {
+            $pathinfo = pathinfo($entry, PATHINFO_EXTENSION);
+            $pathinfo = strtolower($pathinfo);
+
+            if (in_array($pathinfo, $ext)) {
+                $sortedNotWritableFile[] = $entry;
+            }
+        }
+    } else {
+        $sortedNotWritableFile = false;
+    }
+    return array(
+        'file_writable' => $sortedWritableFile,
+        'file_not_writable' => $sortedNotWritableFile
+    );
+}
+/**
+ * Get lowercase Array of tokens in a file
+ *
+ * @param string $filename
+ * @return array
+ */
+function getFileTokens($filename)
+{
+    /*
+    token_get_all() This function not support :
+    - Old notation :  "<?  ?>" and "<% %>"
+    - heredoc syntax
+    - nowdoc syntax (since PHP 5.3.0)
+    */
+    $fileContent = file_get_contents($filename);
+    $fileContent = preg_replace('/<\?([^p=\w])/m', '<?php ', $fileContent); // replace old php tags
+    $token = token_get_all($fileContent);
+    $output = array();
+    $tokenCount = count($token);
+
+    if ($token > 0) {
+        for ($i = 0; $i < $tokenCount; $i++) {
+            if (isset($token[$i][1])) {
+                $output[] .= strtolower($token[$i][1]);
+            }
+        }
+    }
+    $output = array_values(
+        array_unique(array_filter(array_map("trim", $output)))
+    );
+    return $output;
+}
+/**
+ * Compare tokens and return array of matched tokens
+ *
+ * @param array $tokenNeedles
+ * @param array $tokenHaystack
+ * @return array
+ */
+function compareTokens($tokenNeedles, $tokenHaystack)
+{
+
+
+    $output = array();
+    foreach ($tokenNeedles as $tokenNeedle) {
+        if (in_array($tokenNeedle, $tokenHaystack)) {
+            $output[] = $tokenNeedle;
+        }
+    }
+    return $output;
+}
+
+$ext = array(
+    'php',
+    'phps',
+    'pht',
+    'phpt',
+    'phtml',
+    'phar',
+    'php3',
+    'php4',
+    'php5',
+    'php7',
+    'suspected'
+);
+
+$tokenNeedles = array(
+    // Obfuscation
+    'base64_decode',
+    'rawurldecode',
+    'urldecode',
+    'gzinflate',
+    'gzuncompress',
+    'str_rot13',
+    'convert_uu',
+    'htmlspecialchars_decode',
+    'bin2hex',
+    'hex2bin',
+    'hexdec',
+    'chr',
+    'strrev',
+    'goto',
+    'implode',
+    'strtr',
+    'extract',
+    'parse_str', //works like extract if only one argument is given.
+    'substr',
+    'mb_substr',
+    'str_replace',
+    'substr_replace',
+    'preg_replace', // able to do eval on match
+    'exif_read_data',
+    'readgzfile',
+
+    // Shell / Process
+    'eval',
+    'exec',
+    'shell_exec',
+    'system',
+    'passthru',
+    'pcntl_fork',
+    'fsockopen',
+    'proc_open',
+    'popen ',
+    'assert', // identical to eval
+    'posix_kill',
+    'posix_setpgid',
+    'posix_setsid',
+    'posix_setuid',
+    'proc_nice',
+    'proc_close',
+    'proc_terminate',
+    'apache_child_terminate',
+
+    // Server Information
+    'posix_getuid',
+    'posix_geteuid',
+    'posix_getegid',
+    'posix_getpwuid',
+    'posix_getgrgid',
+    'posix_mkfifo',
+    'posix_getlogin',
+    'posix_ttyname',
+    'getenv',
+    'proc_get_status',
+    'get_cfg_var',
+    'disk_free_space',
+    'disk_total_space',
+    'diskfreespace',
+    'getlastmo',
+    'getmyinode',
+    'getmypid',
+    'getmyuid',
+    'getmygid',
+    'fileowner',
+    'filegroup',
+    'get_current_user',
+    'pathinfo',
+    'getcwd',
+    'sys_get_temp_dir',
+    'basename',
+    'phpinfo',
+
+    // Database
+    'mysql_connect',
+    'mysqli_connect',
+    'mysqli_query',
+    'mysql_query',
+
+    // I/O
+    'fopen',
+    'fsockopen',
+    'file_put_contents',
+    'file_get_contents',
+    'url_get_contents',
+    'stream_get_meta_data',
+    'move_uploaded_file',
+    '$_files',
+    'copy',
+    'include',
+    'include_once',
+    'require',
+    'require_once',
+    '__file__',
+
+    // Miscellaneous
+    'mail',
+    'putenv',
+    'curl_init',
+    'tmpfile',
+    'allow_url_fopen',
+    'ini_set',
+    'set_time_limit',
+    'session_start',
+    'symlink',
+    '__halt_compiler',
+    '__compiler_halt_offset__',
+    'error_reporting',
+    'create_function',
+    'get_magic_quotes_gpc',
+    '$auth_pass',
+    '$password',
+);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP File Scanner</title>
-    <style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Malware Finder</title>
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap');
+
+        body {
+            font-family: 'Ubuntu Mono', monospace;
+            background-color: #121212;
+            color: #cfcfcf;
+        }
+
         table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        table, th, td {
-            border: 1px solid black;
-        }
-        th, td {
+            border-spacing: 0;
             padding: 10px;
-            text-align: left;
+            border-radius: 7px;
+            border: 3px solid #424242;
+            background-color: #1e1e1e;
+            position: relative;
+        }
+
+        tr,
+        td {
+            padding: 7px;
+        }
+
+        th {
+            color: #ffffff;
+            padding: 7px;
+            font-size: 25px;
+            position: relative;
+        }
+
+        .corner-text {
+            position: absolute;
+            top: -10px;
+            right: -5px;
+            padding: 10px;
+            font-size: 16px;
+            color: #ff9999;
+            background-color: #1e1e1e;
+        }
+
+        input[type=submit]:focus {
+            background: #ff9999;
+            color: #ffffff;
+            border: 3px solid #ff9999;
+        }
+
+        input[type=submit]:hover {
+            border: 3px solid #ff9999;
+            cursor: pointer;
+        }
+
+        input[type=text]:hover {
+            border: 3px solid #ff9999;
+        }
+
+        input[type=button]:hover {
+            border: 3px solid #ff9999;
+        }
+
+        input {
+            font-family: 'Ubuntu Mono', monospace;
+        }
+
+        input[type=text] {
+            border: 3px solid #424242;
+            outline: none;
+            padding: 7px;
+            color: #cfcfcf;
+            background-color: #1e1e1e;
+            width: 100%;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            color: #cfcfcf;
+            border: 3px solid #424242;
+            outline: none;
+            background: none;
+            padding: 7px;
+            width: 100%;
+            border-radius: 7px;
+        }
+
+        input[type=button] {
+            color: #cfcfcf;
+            border: 3px solid #424242;
+            outline: none;
+            background: none;
+            padding: 7px;
+            border-radius: 7px;
+            transition: transform 0.1s ease;
+        }
+
+        input[type=button]:active {
+            transform: scale(0.95);
         }
     </style>
 </head>
 <body>
-    <h1>PHP File Scanner</h1>
-    <?php
-    // Direktori yang ingin discan (sesuaikan jika perlu)
-    $directory = __DIR__;
-    $extensions = ['php', 'php5', 'phtml', 'shtMl'];
-    $results = [];
+    <script type="text/javascript">
+        function copytable(el) {
+            var urlField = document.getElementById(el);
+            var range = document.createRange();
+            range.selectNode(urlField);
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            var button = document.getElementById('copyButton');
+            button.value = 'Text Copied!';
+            setTimeout(function() {
+                 button.value = 'Copy to Clipboard';
+                }, 2000);
+        }
+    </script>
+    <form method="post">
+        <table align="center" width="95%">
+            <tr>
+                <th>
+                    Malware Finder
+                    <div class="corner-text">0xHax0r
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" name="dir" value="<?= getcwd() ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="SEARCH">
+                </td>
+            </tr>
 
-    // Fungsi untuk scan file dalam direktori
-    function scanDirectory($dir)
-    {
-        global $extensions, $results;
-        $files = scandir($dir);
+            <?php if (isset($_POST['submit'])) { ?>
+                <tr>
+                    <td>
+                        <span style="font-weight:bold;font-size:25px;">RESULT</span>
+                        <input type=button value="Copy to Clipboard" id="copyButton" onClick="copytable('result')">
+                    </td>
+                </tr>
+        </table><br>
+        <table id="result" align="center" width="95%">
+            <?php
+            $path = $_POST['dir'];
+            $result = getSortedByExtension($path, $ext);
 
-        foreach ($files as $file) {
-            if ($file === '.' || $file === '..') continue;
+            $fileWritable = $result['file_writable'];
+            $fileNotWritable = $result['file_not_writable'];
+            $fileWritable = sortByLastModified($fileWritable);
 
-            $path = $dir . DIRECTORY_SEPARATOR . $file;
+            foreach ($fileWritable as $file) {
+                $filePath = str_replace('\\', '/', $file);
+                $tokens = getFileTokens($filePath);
+                $cmp = compareTokens($tokenNeedles, $tokens);
+                $cmp = implode(', ', $cmp);
 
-            if (is_dir($path)) {
-                scanDirectory($path);
-            } else {
-                $ext = pathinfo($file, PATHINFO_EXTENSION);
-                if (in_array($ext, $extensions)) {
-                    scanFile($path);
+                if (!empty($cmp)) {
+                    echo sprintf('<tr><td><span style="color:red;">%s (%s)</span></td></tr>', $filePath, $cmp);
+                    //unlink($filePath);
                 }
             }
         }
-    }
-
-    // Fungsi untuk memindai file
-    function scanFile($file)
-    {
-        global $results;
-        $content = file_get_contents($file);
-        $moveUploadedFile = strpos($content, 'move_uploaded_file') !== false;
-        $isEncrypted = preg_match('/(eval\(|base64_decode\(|gzinflate\(|str_rot13\(|gzuncompress\(|pack\()/i', $content);
-
-        if ($moveUploadedFile || $isEncrypted) {
-            $results[] = [
-                'file' => $file,
-                'move_uploaded_file' => $moveUploadedFile,
-                'encrypted_code' => $isEncrypted,
-            ];
-        }
-    }
-
-    // Aksi hapus file jika tombol hapus ditekan
-    if (isset($_GET['delete'])) {
-        $fileToDelete = $_GET['delete'];
-        if (file_exists($fileToDelete)) {
-            unlink($fileToDelete);
-            echo "<p>File " . htmlspecialchars($fileToDelete) . " telah dihapus.</p>";
-        }
-    }
-
-    // Menjalankan scan
-    scanDirectory($directory);
-
-    // Tampilkan hasil scan dalam tabel
-    if (!empty($results)) {
-        echo "<h2>Hasil Scan:</h2>";
-        echo "<table>
-                <tr>
-                    <th>File</th>
-                    <th>move_uploaded_file</th>
-                    <th>Encrypted Code</th>
-                    <th>Aksi</th>
-                </tr>";
-        foreach ($results as $result) {
-            $file = htmlspecialchars($result['file']);
-            $moveUploadedFile = $result['move_uploaded_file'] ? 'Ya' : 'Tidak';
-            $isEncrypted = $result['encrypted_code'] ? 'Ya' : 'Tidak';
-            echo "<tr>
-                    <td>$file</td>
-                    <td>$moveUploadedFile</td>
-                    <td>$isEncrypted</td>
-                    <td>
-                        <a href='" . $file . "' target='_blank'>Lihat</a> | 
-                        <a href='?delete=" . urlencode($file) . "' onclick=\"return confirm('Apakah Anda yakin ingin menghapus file ini?');\">Hapus</a>
-                    </td>
-                  </tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<p><strong>Tidak ada file mencurigakan yang ditemukan.</strong></p>";
-    }
-    ?>
+            ?>
+        </table>
+    </form>
 </body>
 </html>
